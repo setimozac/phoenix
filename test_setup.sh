83e2aae5daa4163f5f7260fe1764a110a649e87a
moe <sizdah@gmail.com>
2025-01-08 19:04:00 -0500

    Added k8s manifests

--- test_setup.sh
+++ test_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-exec > ~/instalation_log.log 2>&1
+# exec > ~/instalation_log.log 2>&1
 cd ~/
+sudo yum -y install git
 export CURRENT_USER=$(whoami)
 sudo yum -y install docker
 sudo usermod -a -G docker $CURRENT_USER
@@ -16,10 +17,14 @@ curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin
 echo "install kind"
-go install sign.k8s.io/kind@v0.26.0
+go install sigs.k8s.io/kind@v0.26.0
 sudo cp ./go/bin/kind /usr/local/bin
 echo "create cluster"
 kind create cluster --name phoenix-dev
 kubectl cluster-info --context kind-operator-dev
+
+
+docker build -t phoenix-operator:latest .
+kind load docker-image phoenix-operator:latest --name phoenix-dev
 
 
